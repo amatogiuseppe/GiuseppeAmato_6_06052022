8d4d9a9f8b1e918a37caab733a9de9b5b1c9aba3
Giuseppe Amato <amato.ct@gmail.com>
2022-05-06 22:57:15 +0200

    Database configuration: Mongoose installation

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://joe:lxdWWx7loKn9ul9v@cluster0.olfbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
-app.use((req, res) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+/*
+app.use(express.json());
+
+app.post('/', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({ message: 'Objet crée !' });
+  next();
+})
+*/
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Votre requête a bien été reçue !' });
 });
 
 module.exports = app;
