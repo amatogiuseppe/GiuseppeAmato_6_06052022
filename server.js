307332ed217b81191d5e70c2ef461c95e9f2dee9
Giuseppe Amato <amato.ct@gmail.com>
2022-05-06 20:43:58 +0200

    Installing the Express application. Also, server.js is now more stable

--- server.js
+++ server.js
@@ -1,7 +1,50 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
+// The normalizePort function returns a valid port, whether supplied as a number or a string
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// The errorHandler function searches for various errors and handles them appropriately
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+// An event listener that logs the port or named channel on which the server is running in the console
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(port);
